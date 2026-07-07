49fbdeb892890c029920aa5a09afe4be0081a6db
kikeconk <bhatkivan@yahoo.com>
2026-07-07 14:41:22 -0500

    Corregir configuración de archivos estáticos

--- lapaznoesblanca/settings.py
+++ lapaznoesblanca/settings.py
@@ -115,8 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
-
 # Configuración para archivos multimedia (Mapas JPG)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
