99d9f9e1c2fb20e0153193e1c9e733447b702dc4
kikeconk <bhatkivan@yahoo.com>
2026-07-09 08:37:33 -0500

    Config: Forzar almacenamiento en Cloudinary con dependencias correctas

--- lapaznoesblanca/settings.py
+++ lapaznoesblanca/settings.py
@@ -26,12 +26,14 @@ ALLOWED_HOSTS = [
 
 # Application definition
 INSTALLED_APPS = [
+    "cloudinary_storage",
+    "cloudinary",
+    "django.contrib.staticfiles",    
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
     "mapas", # Tu aplicación de mapas temporales y territoriales
 ]
 
